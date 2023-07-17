ba2075cb1e26439644eedb7b824a37cd04405453
Christopher Lacasse <clacasse@dropbox.com>
2023-07-17 16:06:08 -0400

    Add create table test, add junk creds

--- itest/integration_test.go
+++ itest/integration_test.go
@@ -1,28 +1,74 @@
 package itest
 
 import (
-	"github.com/aws/aws-sdk-go/aws/session"
 	"net/http/httptest"
-	handler "rajatgoel/dynovault/handler"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/stretchr/testify/require"
+
+	"rajatgoel/dynovault/handler"
 )
 
-func TestListTables(t *testing.T) {
+func getDDBService() (*dynamodb.DynamoDB, error) {
 	ts := httptest.NewServer(handler.New())
 
-	cfg := aws.NewConfig().WithRegion("us-east-1").WithEndpoint(ts.URL)
-	sess, err := session.NewSession(cfg)
-	require.NoError(t, err)
+	cfg := aws.NewConfig()
+	cfg = cfg.WithRegion("us-east-1")
+	cfg = cfg.WithEndpoint(ts.URL)
+	cfg = cfg.WithCredentials(credentials.NewStaticCredentials(
+		"ID",
+		"SECRET_KEY",
+		"TOKEN",
+	))
 
-	ddbSvc := dynamodb.New(sess, cfg)
+	sess, err := session.NewSession(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return dynamodb.New(sess, cfg), nil
+}
+
+func TestListTables(t *testing.T) {
+	ddbSvc, err := getDDBService()
+	require.NoError(t, err)
 
 	// Build the request with its input parameters
 	_, err = ddbSvc.ListTables(&dynamodb.ListTablesInput{
 		Limit: aws.Int64(5),
+	})
+	require.NoError(t, err)
+}
+
+func TestCreateTable(t *testing.T) {
+	ddbSvc, err := getDDBService()
+	require.NoError(t, err)
+
+	tableName := "TestTable"
+	attributeName := "TestAttribute"
+	attributeType := dynamodb.ScalarAttributeTypeS
+	keyType := dynamodb.KeyTypeHash
+
+	var attributeDefinitions []*dynamodb.AttributeDefinition
+	attributeDefinitions = append(attributeDefinitions, &dynamodb.AttributeDefinition{
+		AttributeName: &attributeName,
+		AttributeType: &attributeType,
+	})
+
+	var keySchema []*dynamodb.KeySchemaElement
+	keySchema = append(keySchema, &dynamodb.KeySchemaElement{
+		AttributeName: &attributeName,
+		KeyType:       &keyType,
+	})
+
+	_, err = ddbSvc.CreateTable(&dynamodb.CreateTableInput{
+		TableName:            &tableName,
+		AttributeDefinitions: attributeDefinitions,
+		KeySchema:            keySchema,
 	})
 	require.NoError(t, err)
 }
