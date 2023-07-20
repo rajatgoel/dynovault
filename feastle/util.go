5ce743342560451c59423556628de9aafbe9773b
Christopher Lacasse <chris@lacas.se>
2023-07-20 16:04:10 -0400

    Util funcs for generating features

--- feastle/util.go
+++ feastle/util.go
@@ -1,12 +1,19 @@
 package feastle
 
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
 type FeastFeature struct {
+	FeatureName    string
 	EntityId       string
 	EventTimestamp string
 	Values         map[string][]byte
 }
 
 func GenerateRandomFeature() *FeastFeature {
+	featureName := "todo"
 	randId := "todo"
 	randTs := "todo"
 	randValue1 := "todo"
@@ -14,12 +21,40 @@ func GenerateRandomFeature() *FeastFeature {
 	randValue3 := "todo"
 
 	return &FeastFeature{
+		FeatureName:    featureName,
 		EntityId:       randId,
 		EventTimestamp: randTs,
 		Values: map[string][]byte{
 			"key1": []byte(randValue1),
 			"key2": []byte(randValue2),
 			"key3": []byte(randValue3),
+		},
+	}
+}
+
+func (f *FeastFeature) ddbItem() map[string]*dynamodb.AttributeValue {
+	values := map[string]*dynamodb.AttributeValue{}
+	for k, v := range f.Values {
+		values[k] = &dynamodb.AttributeValue{B: v}
+	}
+	item := map[string]*dynamodb.AttributeValue{
+		"entity_id": &dynamodb.AttributeValue{S: aws.String(f.EntityId)},
+		"event_ts":  &dynamodb.AttributeValue{S: aws.String(f.EventTimestamp)},
+		"values":    &dynamodb.AttributeValue{M: values},
+	}
+	return item
+}
+
+func NewBatchWriteItemInput(tableName string, feature *FeastFeature) *dynamodb.BatchWriteItemInput {
+	return &dynamodb.BatchWriteItemInput{
+		RequestItems: map[string][]*dynamodb.WriteRequest{
+			tableName: {
+				{
+					PutRequest: &dynamodb.PutRequest{
+						Item: feature.ddbItem(),
+					},
+				},
+			},
 		},
 	}
 }
--- feastle/util_test.go
+++ feastle/util_test.go
@@ -9,6 +9,15 @@ import (
 
 func TestGenerateRandomFeature(t *testing.T) {
 	feature := GenerateRandomFeature()
-	fmt.Printf("Feature %s\n", feature)
+	fmt.Printf("Feature: %s\n", feature)
 	require.Equal(t, feature.EntityId, "todo")
+}
+
+func TestNewBatchWriteItemInput(t *testing.T) {
+	feature := GenerateRandomFeature()
+	input := NewBatchWriteItemInput("TestTable", feature)
+	fmt.Printf("Feature as write input: %s\n", input)
+	for tableName := range input.RequestItems {
+		require.Equal(t, tableName, "TestTable")
+	}
 }
